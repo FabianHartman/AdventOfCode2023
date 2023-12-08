d8e3224ee1b8212c9926176c7c46fc5062d1b3af
FabianHartman <fabian.hartman@student.hu.nl>
2023-12-08 12:49:50 +0100

    day8a.py - more flexibel, you can now provide begin and endpoint

--- Day08/day08a.py
+++ Day08/day08a.py
@@ -1,6 +1,7 @@
 class Node:
     all_nodes = {}
-    def __init__(self,name,to_left,to_right):
+
+    def __init__(self, name, to_left, to_right):
         self.to_right = to_right
         self.to_left = to_left
         self.name = name
@@ -13,20 +14,22 @@ class Node:
 
     @staticmethod
     def parseNode(input_node_string):
-        input_node_parts = input_node_string.replace("=","").replace("(","").replace(")","").replace(",","").replace("  "," ").split(" ")
-        return Node(input_node_parts[0],input_node_parts[1],input_node_parts[2])
+        input_node_parts = input_node_string.replace("=", "").replace("(", "").replace(")", "").replace(",",
+                                                                                                        "").replace(
+            "  ", " ").split(" ")
+        return Node(input_node_parts[0], input_node_parts[1], input_node_parts[2])
 
     def __str__(self):
         return f"{self.name} = ({self.to_left},{self.to_right})"
 
 
 class Instructions:
-    def __init__(self,instructions):
+    def __init__(self, instructions):
         self.instructions = instructions
 
-    def getPlacesToGoToZZZ(self,instructions):
+    def getPlacesToGoFromAAAToZZZ(self, instructions, begin, end):
         visited_nodes = []
-        current_node = Node.all_nodes['AAA']
+        current_node = Node.all_nodes[begin]
         visited_nodes.append(current_node)
         while True:
             for instruction in instructions.instructions:
@@ -35,13 +38,15 @@ class Instructions:
                 if instruction == "R":
                     current_node = Node.all_nodes[current_node.to_right]
                 visited_nodes.append(current_node)
-                if current_node.name == 'ZZZ':
+                if current_node.name == end:
                     return visited_nodes
-    def getAmountOfSetpsToGoToZZZ(self,instructions):
-        return len(self.getPlacesToGoToZZZ(instructions))-1
+
+    def getAmountOfSetpsToGoFromAAAToZZZ(self, instructions, begin, end):
+        return len(self.getPlacesToGoFromAAAToZZZ(instructions, begin, end)) - 1
+
 
 with open('./input.txt', 'r') as file:
     inputStrings = file.read().splitlines()
     instructions = Instructions(inputStrings[0])
     Node.parseNodes(inputStrings[2:])
-    print(instructions.getAmountOfSetpsToGoToZZZ(instructions))
+    print(instructions.getAmountOfSetpsToGoFromAAAToZZZ(instructions, 'AAA', 'ZZZ'))
