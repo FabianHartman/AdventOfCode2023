e56a73379e01af7cb95efcba2b8aa4f2c8da4f90
FabianHartman <fabian.hartman@student.hu.nl>
2023-12-12 21:45:45 +0100

    day12b.py - did some refactoring

--- Day12/day12b.py
+++ Day12/day12b.py
@@ -2,9 +2,9 @@ from functools import cache
 
 
 class Row:
-    def __init__(self, pattern, counts):
-        self.pattern = pattern
-        self.counts = counts
+    def __init__(self, records, amounts_of_broken):
+        self.records = records
+        self.amounts_of_broken = amounts_of_broken
 
     @staticmethod
     def parseInputStrings(inputStrings):
@@ -19,35 +19,35 @@ class Row:
 
 class RowArrangementCalculator:
     def __init__(self, records):
-        self.records = records
+        self.rows = records
 
     @cache
-    def calculate_arrangements(self, pattern, counts):
-        if not pattern:
-            return len(counts) == 0
+    def calculate_arrangements(self, records, amounts_of_broken):
+        if not records:
+            return len(amounts_of_broken) == 0
 
-        if not counts:
-            return "#" not in pattern
+        if not amounts_of_broken:
+            return "#" not in records
 
         result = 0
 
-        if pattern[0] in ".?":
-            result += self.calculate_arrangements(pattern[1:], counts)
+        if records[0] in ".?":
+            result += self.calculate_arrangements(records[1:], amounts_of_broken)
 
         if (
-                pattern[0] in "#?"
-                and counts[0] <= len(pattern)
-                and "." not in pattern[: counts[0]]
-                and (counts[0] == len(pattern) or pattern[counts[0]] != "#")
+                records[0] in "#?"
+                and amounts_of_broken[0] <= len(records)
+                and "." not in records[: amounts_of_broken[0]]
+                and (amounts_of_broken[0] == len(records) or records[amounts_of_broken[0]] != "#")
         ):
-            result += self.calculate_arrangements(pattern[counts[0] + 1:], counts[1:])
+            result += self.calculate_arrangements(records[amounts_of_broken[0] + 1:], amounts_of_broken[1:])
 
         return result
 
     def calculate_total_arrangements(self):
         total = 0
-        for row in self.records:
-            total += self.calculate_arrangements(row.pattern, row.counts)
+        for row in self.rows:
+            total += self.calculate_arrangements(row.records, row.amounts_of_broken)
         return total
 
 
