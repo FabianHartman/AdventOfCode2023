a64c0faf87d7ef4f287d8150841601c9d237419d
FabianHartman <fabian.hartman@student.hu.nl>
2023-12-16 22:41:16 +0100

    day15a.py

--- Day15/day15a.py
+++ Day15/day15a.py
@@ -1,3 +1,32 @@
+class Appendix:
+    def __init__(self,sequences):
+        self.sequences = sequences
+
+    @staticmethod
+    def parseInput(input):
+        sequences = input.split(",")
+        return Appendix(sequences)
+
+    def __str__(self):
+        return ",".join(self.sequences)
+
+    def getSumHashScore(self):
+        def getHashScore(sequence):
+            current_value = 0
+            for character in sequence:
+                current_value += ord(character)
+                current_value *= 17
+                current_value %= 256
+            return current_value
+        sum_hash_score = 0
+        for sequence in self.sequences:
+            starting_score = getHashScore(sequence)
+            sum_hash_score += starting_score
+        return sum_hash_score
+
+
+
+
 with open('./input.txt', 'r') as file:
-    for line in file.read().splitlines():
-        continue
+    appendix = Appendix.parseInput(file.read())
+    print(appendix.getSumHashScore())
